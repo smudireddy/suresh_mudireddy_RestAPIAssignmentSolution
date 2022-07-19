6ecf40576d099d53844e8bf3d0122257e41b52c6
Suresh Mudireddy <sureshmudireddy@Sureshs-MacBook-Pro.local>
2022-07-19 12:50:37 +0530

    SQL bootstrapping data for h2 DB added

--- EmployeeManager/src/main/java/com/iitr/gl/employeemanager/security/GLUserDetails.java
+++ EmployeeManager/src/main/java/com/iitr/gl/employeemanager/security/GLUserDetails.java
@@ -45,17 +45,17 @@ public class GLUserDetails implements UserDetails {
 
 	@Override
 	public boolean isAccountNonExpired() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		return false;
+		return true;
 	}
 
 	@Override
--- EmployeeManager/src/main/resources/data.sql
+++ EmployeeManager/src/main/resources/data.sql
@@ -1,8 +1,12 @@
 insert into roles values(1,'USER');
 insert into roles values(2,'ADMIN');
 
-insert into users(user_id, user_name, user_password) values(1,'suresh','$2a$10$nVVbXhUHgRoURTVdkr1kw.SbNBp9R6GbADF/6AORYd/Exv3lwye6a')
-insert into users(user_id, user_name, user_password) values(2,'ramesh','$2a$10$Gwh4H33UX3waS6tScXBww.06GHAh4EIuc0RbQGG0exUrwLzmO0kmG')
+insert into users(user_id, user_name, user_password) values(1,'suresh','$2a$10$nVVbXhUHgRoURTVdkr1kw.SbNBp9R6GbADF/6AORYd/Exv3lwye6a');
+insert into users(user_id, user_name, user_password) values(2,'ramesh','$2a$10$Gwh4H33UX3waS6tScXBww.06GHAh4EIuc0RbQGG0exUrwLzmO0kmG');
+
+insert into users_roles values(1,1);
+insert into users_roles values(1,2);
+insert into users_roles values(2,1);
 
 insert into employee(emp_id,first_name,last_name,email) values(1,'Suresh','Reddy','suresh.mudireddy@gmail.com');
 insert into employee(emp_id,first_name,last_name,email) values(2,'Ramesh','Reddy','ramesh.mudireddy@gmail.com');
