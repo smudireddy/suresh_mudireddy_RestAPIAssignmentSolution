9d94742cc73c6dc668b174001d60ac4b03ff8b08
Suresh Mudireddy <sureshmudireddy@Sureshs-MacBook-Pro.local>
2022-07-20 09:53:59 +0530

    user password encoding changes

--- EmployeeManager/src/main/java/com/iitr/gl/employeemanager/controllers/UserManagementController.java
+++ EmployeeManager/src/main/java/com/iitr/gl/employeemanager/controllers/UserManagementController.java
@@ -1,7 +1,6 @@
 package com.iitr.gl.employeemanager.controllers;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.iitr.gl.employeemanager.entities.Employee;
 import com.iitr.gl.employeemanager.entities.GLUser;
 import com.iitr.gl.employeemanager.entities.GLUserRole;
 import com.iitr.gl.employeemanager.services.UserMaanagementService;
@@ -21,48 +19,48 @@ public class UserManagementController {
 
 	@Autowired
 	private UserMaanagementService userManagementService;
-	
+
 	@Autowired
 	PasswordEncoder passwordEncoder;
-	
+
 	// List of Employees
 	@RequestMapping(value = { "/users" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public List<GLUser> fetchUsersList() {
-		
+
 		List<GLUser> users = userManagementService.fetchAllUsers();
-		
-		for(GLUser user:users) {
+
+		for (GLUser user : users) {
 			user.setPassword("************");
 		}
 		return users;
 	}
-	
+
 	@RequestMapping(value = { "/users/roles" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public List<GLUserRole> fetchUserRolesList() {
-		
+
 		List<GLUserRole> roles = userManagementService.fetchAllRoles();
 		return roles;
 	}
 
-	@RequestMapping(value = { "/users/accessdenied" }, method = { RequestMethod.GET,RequestMethod.PUT, RequestMethod.POST })
+	@RequestMapping(value = { "/users/accessdenied" }, method = { RequestMethod.GET, RequestMethod.PUT,
+			RequestMethod.POST })
 	public String showAccessDenied() {
 		return "Sorry, you do not have permission.";
 	}
-	
-	@RequestMapping(value = {"/users/addrole" }, method = {RequestMethod.POST})
+
+	@RequestMapping(value = { "/users/addrole" }, method = { RequestMethod.POST })
 	public GLUserRole saveEmployee(@RequestBody GLUserRole userRole) {
-		
-		userManagementService.addUserRole(userRole); 
+
+		userManagementService.addUserRole(userRole);
 		return userRole;
 	}
-	
-	@RequestMapping(value = {"/users/adduser" }, method = {RequestMethod.POST})
+
+	@RequestMapping(value = { "/users/adduser" }, method = { RequestMethod.POST })
 	public GLUser saveEmployee(@RequestBody GLUser user) {
-		
+
 		String password = user.getPassword();
 		user.setPassword(passwordEncoder.encode(password));
-		System.out.println("-----> " + user.toString());
-		userManagementService.addUser(user); 
+		userManagementService.addUser(user);
 		return user;
 	}
 }
